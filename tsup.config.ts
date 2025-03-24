c79df1656543dc755be40b11efd497db9712e596
hairyf <wwu710632@gmail.com>
2025-03-24 14:00:17 +0800

    fix: worker reference except by tsup config bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,13 @@ import { dependencies, name } from './package.json'
 
 export default defineConfig(() => {
   return {
-    external: Object.keys(dependencies || {}),
-    entry: ['./src'],
-    format: ['esm', 'cjs'],
+    external: Object.keys({ dependencies }),
+    entry: ['src/**/*.ts', 'src/**/*.js'],
+    format: ['esm'],
     clean: true,
     dts: true,
     splitting: false,
+    bundle: false,
     name,
   }
 })
