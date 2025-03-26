ac3a3cc6edfba68faf67ac96697f79ddfa7c945e
hairyf <wwu710632@gmail.com>
2025-03-26 14:30:48 +0800

    chore: add @vitejs/plugin-vue dependency and update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,10 @@
+import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [
+    vue(),
+  ],
   test: {
     server: {
       deps: {
